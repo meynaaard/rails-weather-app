43d2ddc3c57dedd486130ba9adc998a45fca8d09
meynard <sombritomeynard@gmail.com>
2025-11-08 14:43:31 +0800

    feat: Save forecast information for each request in the database

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,16 +1,28 @@
 class ForecastsController < ApplicationController
   def search
     location = params[:location]
-    if location.blank?
-      render json: { error: "Missing location parameter" }, status: :bad_request
-      return
-    end
+    weather_data = WeatherService.get_forecast(location)
 
-    data = WeatherService.get_forecast(location)
-    if data.nil?
+    if weather_data.nil?
       render json: { error: "Location not found" }, status: :not_found
     else
-      render json: data
+      render json: weather_data
+    end
+
+    location = Location.find_or_create_by(name: weather_data[:location][:name]) do |loc|
+      loc.latitude = weather_data[:location][:latitude]
+      loc.longitude = weather_data[:location][:longitude]
+    end
+
+    daily = weather_data[:daily]
+    daily["time"].each_with_index do |date, i|
+      Forecast.create!(
+        location: location,
+        forecast_date: date,
+        weather_code: daily["weather_code"][i],
+        temp_min: daily["temperature_2m_min"][i],
+        temp_max: daily["temperature_2m_max"][i]
+      )
     end
   end
 end
