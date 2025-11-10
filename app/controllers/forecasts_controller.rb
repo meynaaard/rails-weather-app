187765aca992d782e5caed08f9f1404825046883
meynard <sombritomeynard@gmail.com>
2025-11-10 18:12:54 +0800

    ui: Add daily weather forecast information

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,8 +1,8 @@
 class ForecastsController < ApplicationController
   def search
-    @forecast = WeatherService.get_forecast(params[:location].strip)
+    forecast = WeatherService.get_forecast(params[:location].strip)
 
-    if @forecast.nil?
+    if forecast.nil?
       flash.now[:alert] = "Location not found"
       respond_to do |format|
         format.turbo_stream do
@@ -16,13 +16,13 @@ class ForecastsController < ApplicationController
       return
     end
 
-    location = Location.find_or_create_by(name: @forecast[:location][:name]) do |loc|
-      loc.latitude = @forecast[:location][:latitude]
-      loc.longitude = @forecast[:location][:longitude]
+    location = Location.find_or_create_by(name: forecast[:location][:name]) do |loc|
+      loc.latitude = forecast[:location][:latitude]
+      loc.longitude = forecast[:location][:longitude]
     end
 
-    current = @forecast[:current]
-    forecast = Forecast.create!(
+    current = forecast[:current]
+    @current_forecast = Forecast.create!(
       location: location,
       forecast_date: current["time"],
       weather_code: current["weather_code"],
@@ -32,12 +32,25 @@ class ForecastsController < ApplicationController
       is_day: current["is_day"]
     )
 
+    daily = forecast[:daily]
+    @daily_forecast = daily["time"].each_with_index.map do |date, i|
+      {
+        day: Date.parse(date).strftime("%A"),
+        weather_code: daily["weather_code"][i],
+        temp_max: daily["temperature_2m_max"][i],
+        temp_min: daily["temperature_2m_min"][i]
+      }
+    end
+
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: turbo_stream.replace(
           "forecast",
-          partial: "forecasts/current_forecast",
-          locals: { forecast: @forecast }
+          partial: "forecasts/forecast",
+          locals: {
+            current_forecast: @current_forecast,
+            daily_forecast: @daily_forecast
+          }
         )
       end
     end
--- app/helpers/forecasts_helper.rb
+++ app/helpers/forecasts_helper.rb
@@ -16,9 +16,9 @@ module ForecastsHelper
     65 => { name: "Heavy Rain", icons: { day: "heavy-rain.svg", night: "heavy-rain.svg" } },
     66 => { name: "Light Freezing Rain", icons: { day: "rain.svg", night: "rain.svg" } },
     67 => { name: "Heavy Freezing Rain", icons: { day: "heavy-rain.svg", night: "heavy-rain.svg" } },
-    71 => { name: "Slight snowfall", icons: { day: "light-snow.svg", night: "light-snow-night.svg" } },
-    73 => { name: "Moderate snowfall", icons: { day: "snow.svg", night: "snow.svg" } },
-    75 => { name: "Heavy snowfall", icons: { day: "heavy-snow.svg", night: "heavy-snow.svg" } },
+    71 => { name: "Slight Snowfall", icons: { day: "light-snow.svg", night: "light-snow-night.svg" } },
+    73 => { name: "Moderate Snowfall", icons: { day: "snow.svg", night: "snow.svg" } },
+    75 => { name: "Heavy Snowfall", icons: { day: "heavy-snow.svg", night: "heavy-snow.svg" } },
     77 => { name: "Snow Grains", icons: { day: "snow-grains.svg", night: "snow-grains.svg" } },
     80 => { name: "Slight Rain Showers", icons: { day: "slight-rain-showers.svg", night: "slight-rain-showers-night.svg" } },
     81 => { name: "Moderate Rain Showers", icons: { day: "rain-showers.svg", night: "rain-showers.svg" } },
@@ -41,18 +41,18 @@ module ForecastsHelper
 
   def wind_beaufort_scale(wind_speed)
     case wind_speed
-    when 0..1 then "wind-beaufort-0.svg"
-    when 2..5 then "wind-beaufort-1.svg"
-    when 6..11 then "wind-beaufort-2.svg"
-    when 12..19 then "wind-beaufort-3.svg"
-    when 20..28 then "wind-beaufort-4.svg"
-    when 29..38 then "wind-beaufort-5.svg"
-    when 39..49 then "wind-beaufort-6.svg"
-    when 50..61 then "wind-beaufort-7.svg"
-    when 62..74 then "wind-beaufort-8.svg"
-    when 75..88 then "wind-beaufort-9.svg"
-    when 89..102 then "wind-beaufort-10.svg"
-    when 103..117 then "wind-beaufort-11.svg"
+    when 0..1.9 then "wind-beaufort-0.svg"
+    when 2..5.9 then "wind-beaufort-1.svg"
+    when 6..11.9 then "wind-beaufort-2.svg"
+    when 12..19.9 then "wind-beaufort-3.svg"
+    when 20..28.9 then "wind-beaufort-4.svg"
+    when 29..38.9 then "wind-beaufort-5.svg"
+    when 39..49.9 then "wind-beaufort-6.svg"
+    when 50..61.9 then "wind-beaufort-7.svg"
+    when 62..74.9 then "wind-beaufort-8.svg"
+    when 75..88.9 then "wind-beaufort-9.svg"
+    when 89..102.9 then "wind-beaufort-10.svg"
+    when 103..117.9 then "wind-beaufort-11.svg"
     else "wind-beaufort-12.svg"
     end
   end
